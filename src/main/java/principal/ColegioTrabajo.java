f5ebbb1340ebd466253981fe9ddc358ff4f51d92
ivan <9spotzs@gmail.com>
2025-10-19 17:06:08 -0500

    Agregar el metodo del menu principal para las funciones que habran

--- src/main/java/principal/ColegioTrabajo.java
+++ src/main/java/principal/ColegioTrabajo.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package principal;
 
-/**
- *
- * @author 9spot
- */
+import vista.ColegioVista;
+import controlador.ColegioControlador;
+
 public class ColegioTrabajo {
-    
+    public static void main(String[] args) {
+        ColegioVista vista = new ColegioVista();
+         
+        ColegioControlador controlador = new ColegioControlador(vista);
+        
+        vista.setControlador(controlador);
+        controlador.iniciar();
+    }
 }
