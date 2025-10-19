5f11c5187db210e4e777097fe731f0be6bb62d52
ivan <9spotzs@gmail.com>
2025-10-19 13:39:00 -0500

    Constructor único (solo un constructor en una de las subclases), Llama al constructor vacío de Persona, ademas de añadir sus atributos propios

--- src/main/java/modelo/Estudiante.java
+++ src/main/java/modelo/Estudiante.java
@@ -8,6 +8,21 @@ package modelo;
  *
  * @author 9spot
  */
-public class Estudiante {
-
+public class Estudiante extends Persona {
+    private String grado;
+    private String codigoEstudiante;
+    private double promedio;
+    
+    public Estudiante(String nombre, String direccion, String telefono, 
+                     String fechaNacimiento, String grado, 
+                     String codigoEstudiante, double promedio) {
+        super(); 
+        this.setNombre(nombre);
+        this.setDireccion(direccion);
+        this.setTelefono(telefono);
+        this.setFechaNacimiento(fechaNacimiento);
+        this.grado = grado;
+        this.codigoEstudiante = codigoEstudiante;
+        this.promedio = promedio;
+    }
 }
